99f2072ed14089f313ffcdd659e0eabe33a03d98
Tsaritsin S.S. <tsaritsin@biruza.com>
2025-03-21 14:22:00 +0200

    Added new packages

--- Source/ScriptDeployTools.Sources.Embedded/EmbeddedSourceOptions.cs
+++ Source/ScriptDeployTools.Sources.Embedded/EmbeddedSourceOptions.cs
@@ -3,12 +3,19 @@ using System.Text;
 
 namespace ScriptDeployTools.Sources.Embedded;
 
+/// <summary>
+/// Represents configuration options for specifying and filtering embedded resources, such as assemblies, filters,
+/// and encoding types.
+/// </summary>
 public class EmbeddedSourceOptions
 {
     #region Assemblies
 
     private List<Assembly> _assemblies = [];
 
+    /// <summary>
+    /// Gets or sets the collection of assemblies used to locate embedded resources.
+    /// </summary>
     public IReadOnlyCollection<Assembly> Assemblies
     {
         get => _assemblies;
@@ -21,6 +28,9 @@ public class EmbeddedSourceOptions
 
     private Func<string, bool>? _filter;
 
+    /// <summary>
+    /// Gets or sets a predicate function used to filter embedded resource names.
+    /// </summary>
     public Func<string, bool> Filter
     {
         get => _filter ?? (_ => true);
@@ -33,6 +43,9 @@ public class EmbeddedSourceOptions
 
     private Encoding? _encoding;
 
+    /// <summary>
+    /// Gets or sets the encoding used for reading embedded resources. Defaults to UTF-8 if not explicitly specified.
+    /// </summary>
     public Encoding Encoding
     {
         get => _encoding ??= Encoding.UTF8;
--- Source/ScriptDeployTools.Targets.SqlServer/DeployBuilderExtensions.cs
+++ Source/ScriptDeployTools.Targets.SqlServer/DeployBuilderExtensions.cs
@@ -1,7 +1,16 @@
 ﻿namespace ScriptDeployTools.Targets.SqlServer;
 
+/// <summary>
+/// Provides extension methods for configuring deployment builders to target SQL Server.
+/// </summary>
 public static class DeployBuilderExtensions
 {
+    /// <summary>
+    /// Configures the deployment builder to target a SQL Server database.
+    /// </summary>
+    /// <param name="builder">The deployment builder to configure.</param>
+    /// <param name="applyOptions">An action to configure the SQL Server target options.</param>
+    /// <returns>The configured deployment builder.</returns>
     public static IDeployBuilder ToSqlServer(this IDeployBuilder builder,
                                              Action<SqlServerTargetOptions> applyOptions)
     {
